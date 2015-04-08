d57d18de6415ef21f62c4df2fe3afdf617f5ac08
林浩特 <htlin@x-one.cc>
2015-04-08 22:44:12 +0800

    修改路径。

--- tangxi_website/settings.py
+++ tangxi_website/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#t=r&q_rhb5qu=x=545(r$^m$&06(au^yrr@mrc8i+41)p80*!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
